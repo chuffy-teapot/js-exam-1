28c82aa25914714b31a326900f94da08e546158b
chuffy <98713993+chuffy-teapot@users.noreply.github.com>
2022-05-26 16:56:31 +0500

    second upload

--- js/newNode.js
+++ js/newNode.js
@@ -1,9 +1,8 @@
-export default function ({ id, first_name, last_name, email, avatar }) {
+export default function ({first_name, last_name, email, avatar }) {
   let template = document.querySelector(".template");
   let newTemplate = template.content.cloneNode(true);
 
-  let card = newTemplate.querySelector(".card")
-  card.dataset.id = id;
+  newTemplate.querySelector(".card")
   newTemplate.querySelector(".img").src = avatar;
   newTemplate.querySelector(".name").textContent = `${first_name} ${last_name}`;
   newTemplate.querySelector(".email").textContent = email;
